626d1bf982f43937780555101c3e37aba1052ace
Ghack81 <herve-mention@hotmail.com>
2022-03-19 13:50:50 +0100

    File Bug with PyAutoGUI

--- pyautogui_test.py
+++ pyautogui_test.py
@@ -1,12 +1,13 @@
-import pyautogui
+# Serch for pyautogui
+import PyAutoGUI
 
 
 distance = 200
 
 while distance > 0:
-        pyautogui.drag(distance, 0, duration=0.5)   # move right
+        PyAutoGUI.drag(distance, 0, duration=0.5)   # move right
         distance -= 5
-        pyautogui.drag(0, distance, duration=0.5)   # move down
-        pyautogui.drag(-distance, 0, duration=0.5)  # move left
+        PyAutoGUI.drag(0, distance, duration=0.5)   # move down
+        PyAutoGUI.drag(-distance, 0, duration=0.5)  # move left
         distance -= 5
-        pyautogui.drag(0, -distance, duration=0.5)  # move up
+        PyAutoGUI.drag(0, -distance, duration=0.5)  # move up
