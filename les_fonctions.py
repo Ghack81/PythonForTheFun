382321c1f0e0a39dbe104f08db493ee1d191dec9
Ghack81 <herve-mention@hotmail.com>
2022-03-19 16:51:16 +0100

    New File for the fonctions

---
+++ les_fonctions.py
@@ -0,0 +1,10 @@
+# Les fonctions
+
+def welcome():
+    # instruction appartenant à welcome()
+    print("Bienvenue sur ma chaine Youtube")
+    result = 5 + 6
+    print("Le résultat du calcul est ", result)
+
+
+welcome()
