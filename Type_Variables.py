4ec878c82b962207acc74600bc1b41e320ec8a90
Ghack81 <herve-mention@hotmail.com>
2022-03-19 19:25:57 +0100

    File with variable type v1.1

---
+++ Type_Variables.py
@@ -0,0 +1,25 @@
+# Type de Variables
+
+from turtle import color
+
+
+name = 'Mike' # string (texte)
+print(name)
+
+age = 42 # integer (nombre entier)
+print(age)
+
+price = 199.99 # float (nombre décimal)
+print(price)
+
+is_active = True # boolean (vrai ou faux)
+print(is_active)
+
+colors = ['red', 'green', 'blue'] # liste
+print(colors)
+
+states = ('inactive', 'active', 'archive') # tuple
+print(states)
+
+products = { 'name': 'iPad Pro', 'price': 199.99 } # dictionnaire
+print(products)
