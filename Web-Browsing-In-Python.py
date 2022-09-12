54ff256fe400cc9eb7bedb7ddcf1506553163863
Ghack81 <herve-mention@hotmail.com>
2022-09-12 18:33:02 +0200

    Web Browsing in Python

---
+++ Web-Browsing-In-Python.py
@@ -0,0 +1,13 @@
+#pip install wbbrowser
+
+import webbrowser
+Url=input("Enter a url : ")
+webbrowser.open(Url)
+
+#Open the page in a new browser window.
+webbrowser.open_new('Url')
+
+#Open the page in a new browser tab.
+webbrowser.open_new_tab('Url')
+
+#Source => closing.com 
