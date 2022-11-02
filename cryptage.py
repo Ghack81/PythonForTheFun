c3c70ceb4fb4c74411aa02c2c865dafbdf07ad26
Ghack81 <herve-mention@hotmail.com>
2022-11-02 13:29:46 +0100

    Add new model for crypte

--- cryptage.py
+++ cryptage.py
@@ -9,3 +9,20 @@ for i in range(longueur):
     message_crypte = message_crypte + codage 
 print ("message cryptée à envoyer")
 print(message_crypte)
+
+#-------------------------------------#
+#nom programme : cryptage_leek_speak_plus.py
+
+message_clair = input("tapez le message en clair : ")
+message_code = message_clair
+
+cle = [ ['s','5'], ['t','7'], ['g', '9'], ['h', '4'], ['u','||'], ['e','!'] ]
+
+for elt  in cle:
+    ancien =elt[0]
+    nouveau = elt[1]
+    message_code = message_code.replace(ancien, nouveau)
+
+print('message codé : ' , message_code)
+
+
