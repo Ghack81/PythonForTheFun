8b17bcebe6e46768dd82faae4c3afde215e66be5
Ghack81 <herve-mention@hotmail.com>
2022-11-02 13:34:17 +0100

    function geometry

---
+++ geometry.py
@@ -0,0 +1,13 @@
+#nom prog. : tortue_exemple.py
+from turtle import *
+speed(1)
+forward(200)    
+left(90)
+forward(100)
+right(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(300)
+mainloop()
